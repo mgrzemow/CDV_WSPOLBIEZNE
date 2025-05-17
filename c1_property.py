81da0821fc6f31dd869bf05f5dc96713323aa5a2
Michał Grzemowski <mgrzemow@gmail.com>
2025-05-17 10:47:19 +0200

    c2

---
+++ c1_property.py
@@ -0,0 +1,39 @@
+class Host:
+    __slots__ = ['_ip']
+
+    def _set_ip(self, ip):
+        w = tuple(int(i) for i in ip.split('.'))
+        if len(w) != 4:
+            raise ValueError(f'ip address must have 4 number, got: {ip}')
+        if not all(0 <= i <= 255 for i in w):
+            raise ValueError(f'ip address must be in range 0-255, got: {ip}')
+        self._ip = w
+
+    def _get_ip(self):
+        return '.'.join(str(i) for i in self._ip)
+
+    ip = property(fset=_set_ip, fget=_get_ip)
+
+    @property
+    def ip_tuple(self):
+        return self._ip
+
+    def __init__(self, ip):
+        self.ip = ip
+
+if __name__ == '__main__':
+    h1 = Host('192.168.127.12')
+    print(h1.ip)
+    h1.ip = '8.8.8.8'
+    print(h1.ip)
+    print(h1.ip_tuple)
+    
+    # zamienić ip na property
+    # tak aby przechowywać nr ip w krotce (192,168,0,1)
+    # walidacja - poniższe operacje mają się wywalić z błędem ValueError
+    # 4 człony numeru
+    # h1.ip = '8.8.8'
+    # h1.ip = '8.8.8.8.8'
+    # każdy z członów - z przedziału 0..255
+    # h1.ip = '192.168.0.ala'
+    # h1.ip = '192.168.0.999'
---
+++ c2_deskryptor_wlasny.py
@@ -0,0 +1,39 @@
+class MojDeskryptor:
+    def __init__(self, atr_name=None):
+        self._atr_name = atr_name
+
+    def __get__(self, instance, owner):
+        print(f'__get__({instance}, {owner})')
+        return getattr(instance, self._atr_name)
+
+    def __set__(self, instance, value):
+        setattr(instance, self._atr_name, value)
+        print(f'__set__({instance}, {value})')
+
+    def __set_name__(self, owner, name):
+        if self._atr_name is None:
+            self._atr_name = '_' + name
+
+
+class Czlowiek:
+    # __slots__ = [
+    #     '_imie',
+    #     '_nazwisko',
+    # ]
+
+    imie_nazwisko = MojDeskryptor()
+
+    def __init__(self, imie_nazwisko):
+        self.imie_nazwisko = imie_nazwisko
+
+
+# dopisać pamiętanie historii wartości atrybutu
+# za każdym zapisem niech się wypisuje historia - prosta lista
+
+# uwaga - co będzie jeżeli będą 2 instancje klasy Czlowiek?
+
+if __name__ == '__main__':
+    c1 = Czlowiek('Adam Nowak')
+    print(c1.imie_nazwisko)
+    c1.imie_nazwisko = 'Ala Kowalska'
+    print(c1.imie_nazwisko)
