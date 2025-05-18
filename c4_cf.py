fc9d52bc90474e0d1974654f58e00f2c9c2d8e36
Michał Grzemowski <mgrzemow@gmail.com>
2025-05-18 11:16:22 +0200

    c4_cf

--- c4_cf.py
+++ c4_cf.py
@@ -1,40 +1,34 @@
 # mając dany portfel walutowy:
+import random
 import threading
+import time
+
 import requests
+import concurrent.futures as cf
 
 # policzyć jego wartość w złotówkach za pomocą tylu watków ile walut
 # w każdym z wątków wysłać zapytnie do API NBP:
 # https://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json
+waluty = ['THB', 'USD', 'AUD', 'HKD', 'CAD', 'NZD', 'SGD', 'EUR', 'HUF', 'CHF', 'GBP', 'UAH', 'JPY', 'CZK', 'DKK', 'ISK', 'NOK', 'SEK', 'RON', 'BGN', 'TRY', 'ILS', 'CLP', 'PHP', 'MXN', 'ZAR', 'BRL', 'MYR', 'IDR', 'INR', 'KRW', 'CNY', 'XDR']
+d = {w: random.randint(1000, 5000) for w in waluty}
 
-d = {
-    'usd': 1000,
-    'eur': 200,
-    'jpy': 300,
-    'chf': 2000,
-    'cad': 222,
-    'nzd': 233,
-}
-
-wyniki = {}
-
-def wycen(waluta, kwota):
+def wycen(krotka):
+    waluta, kwota = krotka
     r = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/a/{waluta}/?format=json')
     kurs = r.json()['rates'][0]['mid']
-    wyniki[waluta] = round(kurs * kwota, 2)
+    return round(kurs * kwota, 2)
+
 
 # użyć concurrent futures zamiast watków
 # ThreadPoolExecutor
 
-lista_watkow = []
+if __name__ == '__main__':
+    for n in [1, 2, 4, 8, 16, 32]:
+        with cf.ThreadPoolExecutor(n) as ex:
+            t1 = time.perf_counter()
+            suma = 0
+            for w in ex.map(wycen, d.items()):
+                suma = round(suma + w, 2)
 
-for waluta, kwota in d.items():
-    lista_watkow.append(threading.Thread(target=wycen, args=(waluta, kwota)))
-
-for t in lista_watkow:
-    t.start()
-
-for t in lista_watkow:
-    t.join()
-
-print(wyniki)
-print(round(sum(wyniki.values()), 2))
+            # suma = round(sum(w for w in ex.map(wycen, d.items())), 2)
+            print(f'{n} watkow, czas {time.perf_counter() - t1:.2f} s. wynik: {suma}')
